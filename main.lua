c55bf5612bac0882bb3b5ebe78f1f04a7f930b1b
= <=>
2024-09-07 11:25:46 -0700

    player movement!!!!!!!!!
    Co-authored-by: Wes Le <basedIdiot@users.noreply.github.com>
    Co-authored-by: guaba <stressedGuaba@users.noreply.github.com>

--- main.lua
+++ main.lua
@@ -1,1 +1,14 @@
+local Player = require 'Player'
+
 -- Haiiii justin please send help
+function love.load()
+    player = Player.new(0, 0)
+end
+
+function love.update(dt)
+    player:update(dt)
+end
+
+function love.draw()
+    player:draw()
+end
