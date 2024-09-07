4850e0c748bb5899fd2bda3d0af749eb9de0b659
= <=>
2024-09-07 15:27:43 -0700

    created new npcs and added npc sprites

--- images.lua
+++ images.lua
@@ -1,5 +1,11 @@
 local images = {}
 
 images.player = love.graphics.newImage('images/player_spritesheet.png')
+images.addition = love.graphics.newImage('images/addition.png')
+images.subtraction = love.graphics.newImage('images/subtraction.png')
+images.multiplication = love.graphics.newImage('images/multiplication.png')
+images.division = love.graphics.newImage('images/division.png')
+images.mathSymbol = love.graphics.newImage('images/biblically_accurate_math_symbol.png')
+images.endBossMap = love.graphics.newImage('images/end_boss_map.png')
 
 return images
