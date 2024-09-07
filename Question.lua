675734c08adc5a5433a1fdee016d36d133a6d7fa
Wes Le <88017350+basedIdiot@users.noreply.github.com>
2024-09-07 11:30:45 -0700

    question updatec

--- Question.lua
+++ Question.lua
@@ -26,11 +26,8 @@ function love.keypressed(key)
         if byteoffset then
             -- remove the last UTF-8 character.
             -- string.sub operates on bytes rather than UTF-8 characters, so we couldn't do string.sub(text, 1, -2).
-            text = string.sub(text, 1, byteoffset - 1)
+            currentAnswer = string.sub(text, 1, byteoffset - 1)
         end
-    end
-    if key == "enter" then
-        
     end
 end
 return QuestionManager
