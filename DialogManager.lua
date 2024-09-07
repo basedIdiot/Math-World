852fa90d9dc7e3d61fdf731919bd5ee1e9cc4de1
= <=>
2024-09-07 13:31:19 -0700

    added dialog and changed screen dimensions

---
+++ DialogManager.lua
@@ -0,0 +1,47 @@
+local DialogManager = {}
+DialogManager.__index = DialogManager
+
+function DialogManager.new()
+    local self = setmetatable({}, DialogManager)
+
+    self.textSequence = {}
+
+    return self
+end
+
+function DialogManager:displayText(...)
+    for _, text in ipairs({...}) do
+        table.insert(self.textSequence, text)
+    end
+end
+
+
+function DialogManager:draw()
+    if #self.textSequence > 0 then
+        love.graphics.setColor(0, 0, 0, 0.8)
+        love.graphics.rectangle(
+            'fill',
+            0,
+            love.graphics.getHeight() * 0.6,
+            love.graphics.getWidth(),
+            love.graphics.getHeight() * 0.4
+        )
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.printf(
+            self.textSequence[1],
+            0 + 25, love.graphics.getHeight() * 0.6 + 25,
+            love.graphics.getWidth() - 50,
+            'left',
+            0,
+            2, 2
+        )
+    end
+end
+
+function DialogManager:keypressed(key)
+    if key == 'space' and #self.textSequence > 0 then
+        table.remove(self.textSequence, 1)
+    end
+end
+
+return DialogManager
--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 local Player = require 'Player'
 local QuestionManager = require 'Question'
+local DialogManager   = require 'DialogManager'
 
 -- Haiiii justin please send help
 
@@ -8,6 +9,8 @@ function love.load()
     love.graphics.setBackgroundColor(1, 1, 1)
     
     player = Player.new(0, 0)
+    dialogManager = DialogManager.new()
+    dialogManager:displayText('bruh', 'hehe')
     QuestionManager.askQuestion('bro', 'blud')
 end
 
@@ -17,6 +20,7 @@ end
 
 function love.keypressed(key, scancode)
     QuestionManager.keypressed(scancode)
+    dialogManager:keypressed(scancode)
 end
 
 function love.draw()
@@ -24,4 +28,6 @@ function love.draw()
     love.graphics.setColor(0, 0, 0)
     QuestionManager.draw()
     love.graphics.setColor(1, 1, 1)
+
+    dialogManager:draw()
 end
